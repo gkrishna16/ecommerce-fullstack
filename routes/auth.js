f093a71c07040e3744a93abf66f9b2cea55f928f
Gopal Krishan <gopalkbks@Gopals-MacBook-Pro.local>
2022-01-23 08:15:39 -0800

    sec commit

---
+++ routes/auth.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const User = require('../models/User');
+
+// Register
+router.post('/register', async (req, res) => {
+	const newUser = new User({
+		username: req.body.username,
+		email: req.body.email,
+		password: req.body.password,
+	});
+
+	try {
+		const savedUser = await newUser.save();
+		res.status(200).json(savedUser);
+		console.log(savedUser);
+	} catch (error) {
+		res.status(500).send(error);
+	}
+});
+
+module.exports = router;
